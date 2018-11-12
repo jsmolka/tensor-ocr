b420487eb992e54eafd386f82b5d24b2f8641691
tstrempel <tom.strempel@mailbox.org>
2018-11-12 21:47:43 +0100

    finished model

--- src/model/model2.py
+++ src/model/model2.py
@@ -5,7 +5,9 @@ import keras
 from keras.models import Sequential, Model
 from keras.layers import (Conv2D, MaxPooling2D, LSTM, Flatten, 
 TimeDistributed, Activation, Dense, Input, Lambda, Bidirectional, 
-Dropout, Reshape)
+Dropout, Reshape, GRU)
+from keras.layers.merge import add, concatenate
+from keras.optimizers import SGD
 from keras import backend as K
 from os.path import join, split, splitext
 
@@ -13,7 +15,8 @@ batch_size = 128
 epochs = 5
 rows, cols = 128, 64
 channels = 1
-
+alphabet_size = 78
+label_max_length = 32
 
 def load_iam_data(path, file_iter, size):
     """Parses images into numpy arrays."""
@@ -31,15 +34,16 @@ def load_iam_data(path, file_iter, size):
     return x, y
 
 def ctc_lambda(args):
-    pred_y, labels, input_length, label_length = args
-    return K.ctc_batch_cost(labels, pred_y, input_length, label_length)
+    predict_y, labels, input_length, label_length = args
+    predict_y = predict_y[:, 2:, :]
+    return K.ctc_batch_cost(labels, predict_y, input_length, label_length)
 
 src = input("Source dir: ")
 files = os.listdir(src)
 file_iter = iter(files)
 
-validate_x, validate_y = load_iam_data(src, file_iter, 1)
-train_x, train_y = load_iam_data(src, file_iter, 5)
+validate_x, validate_y = load_iam_data(src, file_iter, 1000)
+train_x, train_y = load_iam_data(src, file_iter, 20000)
 
 if K.image_data_format() == 'channels_first':
     validate_x = validate_x.reshape(validate_x.shape[0], channels, rows, cols)
@@ -61,26 +65,35 @@ train_y = train_y.astype('str')
 train_x /= 255
 
 input_shape = (None, rows, cols, channels)
-input_data = Input(name="input1", shape=input_shape)
+input_data = Input(name="input_data", shape=input_shape)
 model = TimeDistributed(Conv2D(64, kernel_size=(3,3), activation='relu'))(input_data)
 model = TimeDistributed(MaxPooling2D(pool_size=(2,2)))(model)
 model = TimeDistributed(Conv2D(64, kernel_size=(3,3), activation='relu'))(model)
-model = TimeDistributed(Conv2D(64, kernel_size=(3,3), activation='relu'))(model)
 model = TimeDistributed(MaxPooling2D(pool_size=(2,2)))(model)
 model = TimeDistributed(Conv2D(64, kernel_size=(3,3), activation='relu'))(model)
 model = TimeDistributed(MaxPooling2D(pool_size=(2,2)))(model)
 
-conv_to_rnn_dims = (cols // 4, (rows // 4) * 64)
-model = Reshape(target_shape=conv_to_rnn_dims)(model)
+convert_cnn_to_rnn = (cols // 4, (rows // 4) * 64)
+model = Reshape(target_shape=convert_cnn_to_rnn)(model)
+model = Dense(32, activation='relu', name='dense1')(model)
 
-model = LSTM(512)(model)
-pred_y = Dense(78, kernel_initializer='he_normal')(model)
-# pred_y = Activation('softmax')(model)
+gru_1 = GRU(512, return_sequences=True, kernel_initializer='he_normal', name='gru1')(model)
+gru_1b = GRU(512, return_sequences=True, kernel_initializer='he_normal', go_backwards=True, name='gru1b')(model)
+gru1_merged = add([gru_1, gru_1b])
+gru_2 = GRU(512, return_sequences=True, kernel_initializer='he_normal', name='gru2')(gru1_merged)
+gru_2b = GRU(512, return_sequences=True, kernel_initializer='he_normal', go_backwards=True, name='gru2b')(gru1_merged)
 
-labels = Input(shape=[1], dtype='float32')
-input_length = Input(shape=[1], dtype='int64')
-label_length = Input(shape=[1], dtype='int64')
+model = Dense(alphabet_size+1, kernel_initializer='he_normal', name='dense2')(concatenate([gru_2, gru_2b]))
+predict_y = Activation('softmax')(model)
 
-loss_output = Lambda(ctc_lambda)([pred_y, labels, input_length, label_length])
+labels = Input(shape=[label_max_length], dtype='float32', name='labels')
+input_length = Input(shape=[1], dtype='int64', name='input_length')
+label_length = Input(shape=[1], dtype='int64', name='label_length')
 
-model = Model(inputs=input_data, outputs=loss_output)
+loss_output = Lambda(ctc_lambda, output_shape=(1,), name='ctc')([predict_y, labels, input_length, label_length])
+
+model = Model(inputs=[input_data, labels, input_length, label_length], outputs=loss_output)
+
+model.compile(loss={'ctc': lambda true_y, predict_y: predict_y}, optimizer='sgd')
+model.summary()
+
