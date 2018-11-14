0ef43d4c626d37eabad4398d18751aee7f7bccea
tstrempel <tom.strempel@mailbox.org>
2018-11-14 15:41:12 +0100

    finished model

--- src/model/model2.py
+++ src/model/model2.py
@@ -23,14 +23,28 @@ batch_size = 128
 epochs = 5
 rows, cols = 128, 64
 channels = 1
-alphabet =  " !\"#&'()*+,-./0123456789:;?ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+alphabet = " !\"#&'()*+,-./0123456789:;?ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 alphabet_size = len(alphabet)
 label_max_length = 32
 
 def load_iam_data(path, file_iter, size):
     """Parses images into numpy arrays."""
     x = np.ndarray(shape=(size, rows, cols))
-    y = np.ndarray(shape=(size, label_max_length), dtype='int32')
+    y = np.ndarray(shape=(size), dtype=object)
+
+    for i in range(0, size):
+        fi = next(file_iter)
+
+        tmp = cv2.imread(join(path, fi), cv2.IMREAD_GRAYSCALE)
+        tmp = tmp.reshape(rows, cols)
+
+        y[i] = str(fi)[7:-4]
+    return x, y
+
+def load_iam_data_onehot(path, file_iter, size):
+    """Parses images into numpy arrays."""
+    x = np.ndarray(shape=(size, rows, cols))
+    y = np.ones(shape=(size, label_max_length)) * (alphabet_size+1)
 
     for i in range(0, size):
         fi = next(file_iter)
@@ -47,7 +61,7 @@ def ctc_lambda(args):
     return K.ctc_batch_cost(labels, predict_y, input_length, label_length)
 
 def encode_onehot(word):
-    onehot = np.ndarray(shape=(len(word)), dtype='int32')
+    onehot = np.ones(shape=(label_max_length)) * (alphabet_size+1)
     for i in range(0, len(word)):
         for j in range(0, alphabet_size):
             if word[i] == alphabet[j]:
@@ -66,8 +80,8 @@ src = input("Source dir: ")
 files = os.listdir(src)
 file_iter = iter(files)
 
-validate_x, validate_y = load_iam_data(src, file_iter, validate_size)
-train_x, train_y = load_iam_data(src, file_iter, train_size)
+validate_x, validate_y = load_iam_data_onehot(src, file_iter, validate_size)
+train_x, train_y = load_iam_data_onehot(src, file_iter, train_size)
 
 train_x_input_length = np.full(shape=(train_size), fill_value=30, dtype=int)
 
@@ -92,7 +106,6 @@ train_y_length = np.ndarray(shape=(train_size))
 for i, item in enumerate(train_y):
     train_y_length[i] = len(item)
 
-print(input_shape)
 input_data = Input(name="input_data", shape=input_shape, dtype='float32')
 model = Conv2D(cnn_size, kernel_size=kernel_size, activation='relu', kernel_initializer='he_normal')(input_data)
 model = MaxPooling2D(pool_size=(pool_size, pool_size))(model)
@@ -136,7 +149,10 @@ print(train_x_input_length.shape)
 print(train_y_length.shape)
 
 print(encode_onehot("Hallo Welt"))
+print
 print(decode_onehot([0, 1, 2, 23, 45]))
 
-# model.fit([train_x, train_y, train_x_input_length, train_y_length], output)
+for l in model.layers:
+    print(str(l.name) + ": \t\t" + str(l.output_shape))
 
+model.fit([train_x, train_y, train_x_input_length, train_y_length], output)
