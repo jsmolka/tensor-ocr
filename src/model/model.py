59639735205fd4647a73cf4e2b7ee9f9b626307c
tstrempel <tom.strempel@mailbox.org>
2018-11-14 15:51:59 +0100

    general cleanup, tf throws errors

--- src/model/model.py
+++ src/model/model.py
@@ -2,16 +2,31 @@ import numpy as np
 import os
 import cv2
 import keras
-from keras.models import Sequential
-from keras.layers import (Conv2D, MaxPooling2D, LSTM, Flatten, 
-TimeDistributed, Activation, Dense, Input, Lambda)
+from keras.models import Sequential, Model
+from keras.layers import Conv2D, MaxPooling2D, Activation, Dense, Input
+from keras.layers import Lambda, GRU, Reshape
+from keras.layers.merge import add, concatenate
+from keras.optimizers import SGD
 from keras import backend as K
-from os.path import join, split, splitext
+from os.path import join
+
+train_size = 2000
+validate_size = 100
+cnn_size = 16
+rnn_size = 512
+dense_size = 32
+kernel_size = (3, 3)
+pool_size = 2
 
 batch_size = 128
 epochs = 5
 rows, cols = 128, 64
 channels = 1
+alphabet = (" !\"#&'()*+,-./0123456789:;?"
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+            "abcdefghijklmnopqrstuvwxyz")
+alphabet_size = len(alphabet)
+label_max_length = 32
 
 
 def load_iam_data(path, file_iter, size):
@@ -21,27 +36,60 @@ def load_iam_data(path, file_iter, size):
 
     for i in range(0, size):
         fi = next(file_iter)
-        tmp = cv2.imread(join(path, fi))
-        tmp = tmp.reshape(rows, cols, 3)
 
-        for j, row in enumerate(tmp):
-            for k, col in enumerate(row):
-                x[i][j][k] = col[2]
+        tmp = cv2.imread(join(path, fi), cv2.IMREAD_GRAYSCALE)
+        tmp = tmp.reshape(rows, cols)
+
         y[i] = str(fi)[7:-4]
-
     return x, y
 
+
+def load_iam_data_onehot(path, file_iter, size):
+    """Parses images into numpy arrays."""
+    x = np.ndarray(shape=(size, rows, cols))
+    y = np.ones(shape=(size, label_max_length)) * (alphabet_size+1)
+
+    for i in range(0, size):
+        fi = next(file_iter)
+
+        tmp = cv2.imread(join(path, fi), cv2.IMREAD_GRAYSCALE)
+        tmp = tmp.reshape(rows, cols)
+
+        y[i] = encode_onehot(str(fi)[7:-4])
+    return x, y
+
+
 def ctc_lambda(args):
-    pred_y, labels, input_length, label_length = args
-    pred_y = pred_y[:, 2:, :]
-    return K.ctc_batch_cost(labels, pred_y, input_length, label_length)
+    predict_y, labels, input_length, label_length = args
+    predict_y = predict_y[:, 2:, :]
+    return K.ctc_batch_cost(labels, predict_y, input_length, label_length)
+
+
+def encode_onehot(word):
+    onehot = np.ones(shape=(label_max_length)) * (alphabet_size+1)
+    for i in range(0, len(word)):
+        for j in range(0, alphabet_size):
+            if word[i] == alphabet[j]:
+                onehot[i] = j
+                break
+    return onehot
+
+
+def decode_onehot(onehot):
+    word = []
+    for i in range(0, len(onehot)):
+        word.append(alphabet[onehot[i]])
+    word = ''.join(word)
+    return word
 
 src = input("Source dir: ")
 files = os.listdir(src)
 file_iter = iter(files)
 
-validate_x, validate_y = load_iam_data(src, file_iter, 100)
-train_x, train_y = load_iam_data(src, file_iter, 500)
+validate_x, validate_y = load_iam_data_onehot(src, file_iter, validate_size)
+train_x, train_y = load_iam_data_onehot(src, file_iter, train_size)
+
+train_x_input_length = np.full(shape=(train_size), fill_value=30, dtype=int)
 
 if K.image_data_format() == 'channels_first':
     validate_x = validate_x.reshape(validate_x.shape[0], channels, rows, cols)
@@ -60,34 +108,41 @@ train_x = train_x.astype('float32')
 train_y = train_y.astype('str')
 train_x /= 255
 
-cnn = Sequential()
-cnn.add(Conv2D(64, kernel_size=(3,3), activation='relu', input_shape=input_shape))
-cnn.add(MaxPooling2D(pool_size=(2,2)))
-cnn.add(Conv2D(64, kernel_size=(3,3), activation='relu'))
-cnn.add(Conv2D(64, kernel_size=(2,2), activation='relu'))
-cnn.add(MaxPooling2D(pool_size=(2,2)))
-cnn.add(Conv2D(64, kernel_size=(2,2), activation='relu'))
-cnn.add(MaxPooling2D(pool_size=(2,2)))
-cnn.add(Flatten())
-cnn.add(Dense(32))
+train_y_length = np.ndarray(shape=(train_size))
+for i, item in enumerate(train_y):
+    train_y_length[i] = len(item)
 
-model = Sequential()
-model.add(TimeDistributed(cnn))
-model.add(LSTM(64))
-model.add(LSTM(64))
-model.add(Dense(78, kernel_initializer='he_normal'))
-model.add(Activation('softmax'))
+input_data = Input(name="input_data", shape=input_shape, dtype='float32')
+model = Conv2D(cnn_size, kernel_size=kernel_size, activation='relu', kernel_initializer='he_normal')(input_data)
+model = MaxPooling2D(pool_size=(pool_size, pool_size))(model)
+model = Conv2D(cnn_size, kernel_size=kernel_size, activation='relu', kernel_initializer='he_normal')(model)
+model = MaxPooling2D(pool_size=(pool_size, pool_size))(model)
 
-labels = Input(shape=[32], dtype='float32')
-input_length = Input(shape=[1], dtype='int64')
-label_length = Input(shape=[1], dtype='int64')
+convert_cnn_to_rnn = (32, 210)
+model = Reshape(target_shape=convert_cnn_to_rnn)(model)
+model = Dense(dense_size, activation='relu', name='dense1')(model)
 
-model.add(Lambda(ctc_lambda, output_shape=(1,), arguments=[model, labels, input_length, label_length]))
+gru_1 = GRU(rnn_size, return_sequences=True, kernel_initializer='he_normal', name='gru1')(model)
+gru_1b = GRU(rnn_size, return_sequences=True, kernel_initializer='he_normal', go_backwards=True, name='gru1b')(model)
+gru1_merged = add([gru_1, gru_1b])
+gru_2 = GRU(rnn_size, return_sequences=True, kernel_initializer='he_normal', name='gru2')(gru1_merged)
+gru_2b = GRU(rnn_size, return_sequences=True, kernel_initializer='he_normal', go_backwards=True, name='gru2b')(gru1_merged)
 
-model.compile(loss={}, optimizer='sgd', metrics=['accuracy'])
+model = Dense(alphabet_size+1, kernel_initializer='he_normal', name='dense2')(concatenate([gru_2, gru_2b]))
+predict_y = Activation('softmax', name='predict_y')(model)
 
-model.fit(train_x, train_y, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(validate_x, validate_y))
+labels = Input(shape=[label_max_length], dtype='float32', name='labels')
+input_length = Input(shape=[1], dtype='int64', name='input_length')
+label_length = Input(shape=[1], dtype='int64', name='label_length')
 
-score = model.evaluate(validate_x, validate_y, verbose=0)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
+loss_output = Lambda(ctc_lambda, output_shape=(1,), name='ctc')([predict_y, labels, input_length, label_length])
+
+model = Model(inputs=[input_data, labels, input_length, label_length], outputs=loss_output)
+
+model.compile(loss={'ctc': lambda train_y, predict_y: predict_y}, optimizer='sgd')
+model.summary()
+
+output = np.ndarray(shape=train_size)
+
+
+model.fit([train_x, train_y, train_x_input_length, train_y_length], output)
